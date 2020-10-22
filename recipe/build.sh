642b8af4fb6c5cf7ba9d939d34410cb19a9c126b
Hugo Slepicka <slepicka@slac.stanford.edu>
2020-10-22 14:50:51 -0700

    ENH: Adding mpi alternatives to recipe.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
+mpi_arg="OFF"
+if [[ "$mpi" != "nompi" ]]; then
+  mpi_arg="ON"
+fi
+
 mkdir build
 cd build
 
 cmake \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-    -DUSE_MPI=OFF \
+    -DUSE_MPI=${mpi_arg} \
     ..
 
 make -j${CPU_COUNT} install
