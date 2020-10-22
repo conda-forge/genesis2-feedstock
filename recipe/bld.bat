20478e557ec8d50d20cd99d03b59b66ba6beb44a
Hugo Slepicka <slepicka@slac.stanford.edu>
2020-10-22 08:23:39 -0700

    FIX: Try new strategy for Windows build.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,14 @@
+mkdir build
+cd build
+
 cmake -G "MinGW Makefiles" ^
     -D CMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"
-    .
+    ..
 if errorlevel 1 exit 1
 
-cmake --build . --target install
+cmake --build .
+if errorlevel 1 exit 1
+cmake --install
 if errorlevel 1 exit 1
 
 exit 0
