01feb13e18afed59e1841cc637f11a28511197c1
Isuru Fernando <isuruf@gmail.com>
2021-03-19 12:38:46 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,14 +2,18 @@
 
 # for cross compiling using openmpi
 export OPAL_PREFIX=$PREFIX
-# Fix for cmake bug when cross-compiling
-export FFLAGS="$FFLAGS $LDFLAGS"
 
 if [[ "$target_platform" == osx* ]]; then
     # Hack around https://github.com/conda-forge/gfortran_osx-64-feedstock/issues/11
     # Taken from https://github.com/awvwgk/staged-recipes/tree/dftd4/recipes/dftd4
     # See contents of fake-bin/cc1 for an explanation
     export PATH="${PATH}:${RECIPE_DIR}/fake-bin"
+fi
+
+if [[ "$mpi" != "nompi" ]]; then
+    # Fix for cmake bug when cross-compiling
+    export FFLAGS="$FFLAGS $LDFLAGS"
+    export FC=mpifort
 fi
 
 mkdir build
